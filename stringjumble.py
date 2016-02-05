8ab0369aa727c9f63819bbe09fe4f23372fd022b
EthanAdner <ethanda99@gmail.com>
2016-02-05 14:43:35 -0500

    Updated from Brython Server: 2/5/2016 2:43:35 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,6 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+x=["h","e"]
+print(x)
+print(str(x))

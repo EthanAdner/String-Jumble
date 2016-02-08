980d7f2a915187309b6ca97ea0ddd84193c7efd7
EthanAdner <ethanda99@gmail.com>
2016-02-08 14:32:29 -0500

    Updated from Brython Server: 2/8/2016 2:32:29 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,13 +26,13 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 x=str(input("Please enter a string of text (the bigger the better): "))
-x=list(x)
+w=list(x)
 #print(x)
 z=0
 
 #Letters in reverse
-while z<len(x):
-    y=x[len(x)-z-1]
+while z<len(w):
+    y=w[len(w)-z-1]
     print(y,end="")
     z=z+1
     

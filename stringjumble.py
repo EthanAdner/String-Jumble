d2491d6cd6eb7838fe56f82fed4ec13e69ff9d93
EthanAdner <ethanda99@gmail.com>
2016-02-25 14:15:02 -0500

    Updated from Brython Server: 2/25/2016 2:15:02 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,8 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-x=str(input('Please enter a string of text (the bigger the better): You entered: "'))
-print('". Now Jumble it')
+x=str(input('Please enter a string of text (the bigger the better):'))
+print('You entered:"'+x+ '". Now Jumble it')
 w=list(x)
 z=0
 
